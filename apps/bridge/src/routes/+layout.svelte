5540f06564beb116626ef7283e0c0b6e857da53d
Alex Kuzmin <3dwarka@gmail.com>
2025-12-02 00:51:57 +0500

    update font to inter

--- apps/bridge/src/routes/+layout.svelte
+++ apps/bridge/src/routes/+layout.svelte
@@ -10,6 +10,12 @@
 <svelte:head>
 	<title>{m.app_title()}</title>
 	<link rel="icon" href="/logos/untron/icon.svg" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+	/>
 </svelte:head>
 
 <Navbar />
